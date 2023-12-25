096c22fcf526e90354e1a0d3d5b82b562b474155
Amiya-Jha <97188732+Amiya-Jha@users.noreply.github.com>
2023-12-26 01:30:09 +0530

    Added logic to convert time in Hours and Minutes
    Earlier time fetched from API was a 10 digit timestamp. Now time will be displayed in 12 hour format along with the users timezone.

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
+// Enabling and Disabling Dark Mode
+function myfunction() {
+    let element = document.body;
+    element.dataset.bsTheme = element.dataset.bsTheme == "light" ? "dark" : "light";
+}
+    
 const options = {
-	method: "GET",
-	headers: {
-		"X-RapidAPI-Key": "", //Your API KEY
-		"X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
-	},
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "", // Your API KEY
+        "X-RapidAPI-Host": "weather-by-api-ninjas.p.rapidapi.com",
+    },
 };
+
+// Converting 10 digit timestamp fetched from API to hours and minutes format
+const convertTime = (timestamp, timeZone) => {
+    const date = new Date(timestamp * 1000);
+    const options = { hour12: true, hour: '2-digit', minute: '2-digit', timeZoneName: 'short' };
+    const formattedTime = date.toLocaleTimeString('en-US', options);
+    return formattedTime;
+};
+
+
 const getWeather = (city) => {
-	cityName.innerHTML = city;
-	fetch(
-		"https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
-		options
-	)
-		.then((response) => response.json())
-		.then((response) => {
-			console.log(response);
-			cloud_pct.innerHTML = response.cloud_pct;
-			cloud_pct2.innerHTML = response.cloud_pct;
-			temp.innerHTML = response.temp;
-			temp2.innerHTML = response.temp;
-			feels_like.innerHTML = response.feels_like;
-			humidity.innerHTML = response.humidity;
-			min_temp.innerHTML = response.min_temp;
-			max_temp.innerHTML = response.max_temp;
-			wind_speed.innerHTML = response.wind_speed;
-			wind_degrees.innerHTML = response.wind_degrees;
-			sunrise.innerHTML = response.sunrise;
-			sunset.innerHTML = response.sunset;
-		})
-		.catch((err) => console.error(err));
+    cityName.innerHTML = city;
+    fetch(
+        "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+        options
+    )
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+            cloud_pct.innerHTML = response.cloud_pct;
+            temp.innerHTML = response.temp;           
+            humidity.innerHTML = response.humidity;
+            min_temp.innerHTML = response.min_temp;
+            max_temp.innerHTML = response.max_temp;
+            wind_speed.innerHTML = response.wind_speed;
+            wind_degrees.innerHTML = response.wind_degrees;
+
+            // Convert and display sunrise and sunset times in local time zone
+			const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+			sunrise.innerHTML = convertTime(response.sunrise, userTimeZone);
+			sunset.innerHTML = convertTime(response.sunset, userTimeZone);
+
+        })
+        .catch((err) => console.error(err));
 };
+
+
 submit.addEventListener("click", (e) => {
-	e.preventDefault();
-	getWeather(city.value);
+    e.preventDefault();
+    getWeather(city.value);
 });
 
-getWeather("Mumbai");
+getWeather("Mumbai"); //Default city
